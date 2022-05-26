76c05bce44f02cf3444e8fda047112acb44eea88
JSSan97 <j.san@hotmail.co.uk>
2022-05-26 19:09:13 +0100

    Update coco_inception.py

--- code/inception/coco_inception.py
+++ code/inception/coco_inception.py
@@ -1,3 +1,7 @@
 from inception import get_inception_score
+from miscc.config import cfg
 
 def save_imgs_as_numpy_array():
+    mean, std = get_inception_score(images=cfg.B_VALIDATION_IMG_ARRAY)
+    print(mean)
+    print(std)
