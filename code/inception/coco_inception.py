1e79dba0d07b3546e856ba3e836b471a7f59eee1
JSSan97 <j.san@hotmail.co.uk>
2022-05-26 19:16:36 +0100

    Update coco_inception.py

--- code/inception/coco_inception.py
+++ code/inception/coco_inception.py
@@ -1,7 +1,12 @@
 from inception import get_inception_score
-from miscc.config import cfg
 
-def save_imgs_as_numpy_array():
-    mean, std = get_inception_score(images=cfg.B_VALIDATION_IMG_ARRAY)
+def save_imgs_as_numpy_array(path):
+    eval = np.load(path, allow_pickle=True)
+    eval = np.ndarray.tolist(eval)
+    images = eval['validation_imgs']
+    mean, std = get_inception_score(images=images)
     print(mean)
     print(std)
+
+if __name__ == "__main__":
+    save_imgs_as_numpy_array('/content/drive/MyDrive/Github/Evolutionary_AttnGAN/models/eval_coco_img_array.npy')
