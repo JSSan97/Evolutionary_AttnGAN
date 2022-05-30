7394028be084e045fe54e558cf8f20ba052b659f
JSSan97 <j.san@hotmail.co.uk>
2022-05-30 13:19:55 +0100

    trainer.py to algorithms.trainer.py

--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from __future__ import print_function
 
 from miscc.config import cfg, cfg_from_file
 from datasets import TextDataset
-from trainer import condGANTrainer as trainer
+from algorithms.trainer import condGANTrainer as trainer
 
 import os
 import sys
