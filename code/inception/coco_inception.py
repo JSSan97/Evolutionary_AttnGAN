a3c10bef0d5e261dc7c2c8cd089515607aacbbb6
JSSan97 <j.san@hotmail.co.uk>
2022-05-31 13:04:31 +0100

    Update inception scorer

--- code/inception/coco_inception.py
+++ code/inception/coco_inception.py
@@ -1,13 +1,25 @@
 from inception import get_inception_score
 import numpy as np
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Run inception scorer')
+    parser.add_argument('--file_path', type=str,
+                        default='/content/drive/MyDrive/Github/Evolutionary_AttnGAN/models/eval_coco_img_array.npy')
+    args = parser.parse_args()
+    return args
+
 
 def save_imgs_as_numpy_array(path):
     eval = np.load(path, allow_pickle=True)
     eval = np.ndarray.tolist(eval)
     images = eval['validation_imgs']
     mean, std = get_inception_score(images=images)
+    print("==== Mean ====")
     print(mean)
+    print("==== Standard Deviation ====")
     print(std)
 
 if __name__ == "__main__":
-    save_imgs_as_numpy_array('/content/drive/MyDrive/Github/Evolutionary_AttnGAN/models/eval_coco_img_array.npy')
+    args = parse_args()
+    save_imgs_as_numpy_array(args.file_path)
