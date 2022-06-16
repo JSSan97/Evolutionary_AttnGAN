c807a13ae03f5b99dba8bd2433a72adad443327b
JSSan97 <j.san@hotmail.co.uk>
2022-06-16 17:21:16 +0100

    Update ie_attngan_trainer.py

--- code/algorithms/ie_attngan_trainer.py
+++ code/algorithms/ie_attngan_trainer.py
@@ -277,8 +277,6 @@ class ImprovedEvoTraining(GenericTrainer):
             index = index - len(mutations)
             gene = copy.deepcopy(crossover_pop[index])
             gene_optimizer = copy.deepcopy(crossover_optim[index])
-
-            print(crossover_optim[index])
             selected = 'crossover'
         else:
             eval_imgs = gen_imgs_list[index]
