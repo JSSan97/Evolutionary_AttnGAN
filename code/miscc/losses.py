44d083b6d51702f4d8230e12d009a972a77ef800
JSSan97 <j.san@hotmail.co.uk>
2022-06-16 14:58:06 +0100

    Update losses.py

--- code/miscc/losses.py
+++ code/miscc/losses.py
@@ -251,25 +251,30 @@ def evo_generator_loss(netsD, image_encoder, fake_imgs,
 
         # Ranking loss
         if i == (numDs - 1):
-            # words_features: batch_size x nef x 17 x 17
-            # sent_code: batch_size x nef
-            region_features, cnn_code = image_encoder(fake_imgs[i])
-            w_loss0, w_loss1, _ = words_loss(region_features, words_embs,
-                                             match_labels, cap_lens,
-                                             class_ids, batch_size)
-            w_loss = (w_loss0 + w_loss1) * \
-                cfg.TRAIN.SMOOTH.LAMBDA
-            # err_words = err_words + w_loss.data[0]
-
-            s_loss0, s_loss1 = sent_loss(cnn_code, sent_emb,
-                                         match_labels, class_ids, batch_size)
-            s_loss = (s_loss0 + s_loss1) * \
-                cfg.TRAIN.SMOOTH.LAMBDA
-            # err_sent = err_sent + s_loss.data[0]
-
+            w_loss, s_loss = get_word_and_sentence_loss(image_encoder, fake_imgs[i], words_embs, sent_emb, match_labels, cap_lens, class_ids, batch_size)
             errG_total += w_loss + s_loss
             logs += 'w_loss: %.2f s_loss: %.2f ' % (w_loss.item(), s_loss.item())
-    return errG_total, logs, w_loss.item(), s_loss.item()
+    return errG_total, logs
+
+
+def get_word_and_sentence_loss(image_encoder, fake_imgs, words_embs, sent_emb, match_labels, cap_lens, class_ids, batch_size):
+    # words_features: batch_size x nef x 17 x 17
+    # sent_code: batch_size x nef
+    region_features, cnn_code = image_encoder(fake_imgs)
+    w_loss0, w_loss1, _ = words_loss(region_features, words_embs,
+                                     match_labels, cap_lens,
+                                     class_ids, batch_size)
+    w_loss = (w_loss0 + w_loss1) * \
+             cfg.TRAIN.SMOOTH.LAMBDA
+    # err_words = err_words + w_loss.data[0]
+
+    s_loss0, s_loss1 = sent_loss(cnn_code, sent_emb,
+                                 match_labels, class_ids, batch_size)
+    s_loss = (s_loss0 + s_loss1) * \
+             cfg.TRAIN.SMOOTH.LAMBDA
+
+    return w_loss, s_loss
+
 
 # Used in fitness function
 def discriminator_loss_with_logits(netD, real_imgs, fake_imgs, conditions,
