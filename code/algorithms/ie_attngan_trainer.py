507927b291ac144a26ccacd16ec59c66c62af692
JSSan97 <j.san@hotmail.co.uk>
2022-06-16 16:42:19 +0100

    Update ie_attngan_trainer.py

--- code/algorithms/ie_attngan_trainer.py
+++ code/algorithms/ie_attngan_trainer.py
@@ -359,7 +359,7 @@ class ImprovedEvoTraining(GenericTrainer):
             # loss = self.criterion_MSE(gen_samples, disorder_samples).sqrt_()
             loss_samples = loss.reshape(fake_imgs.size(0), -1).mean(1).unsqueeze(0)
             Fd = torch.cat((Fd, loss_samples))
-        Fd = Fd.mean(0)
+        Fd = Fd.mean(0).mean().item()
 
         Fw = -cfg.EVO.WORD_LOSS_LAMBDA * w_loss
         Fs = -cfg.EVO.SENTENCE_LOSS_LAMBDA * s_loss
