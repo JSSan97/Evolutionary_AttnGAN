2e9ffed11a707e1dfe8425ca556bf4a02524d370
JSSan97 <j.san@hotmail.co.uk>
2022-06-28 21:45:49 +0100

    Update cub_inception_score.py

--- code/cub_inception_score.py
+++ code/cub_inception_score.py
@@ -77,11 +77,11 @@ def get_inception_score(images, model_path, batch_size, splits):
 
     while i < num_batches:
         eval_imgs = images[i * batch_size: (i+1) * batch_size]
-        print(eval_imgs)
-        eval_imgs = eval_imgs.type(dtype)
-        eval_imgsv = Variable(eval_imgs)
+        print(eval_imgs.shape)
+        # eval_imgs = eval_imgs.type(dtype)
+        # eval_imgsv = Variable(eval_imgs)
         batch_size_i = eval_imgs.size()[0]
-        preds[i * batch_size:i * batch_size + batch_size_i] = get_pred(eval_imgsv)
+        preds[i * batch_size:i * batch_size + batch_size_i] = get_pred(eval_imgs)
 
     # Now compute the mean kl-div
     split_scores = []
