c329c1cfd9b3ef3a71015904d359cdeee5b75588
JSSan97 <j.san@hotmail.co.uk>
2022-06-29 12:02:24 +0100

    Update cub_inception_score.py

--- code/cub_inception_score.py
+++ code/cub_inception_score.py
@@ -67,7 +67,8 @@ def get_inception_score(images, model_path, batch_size, splits):
     def get_pred(x):
         # x = up(x)
         pred = model(x)
-        return F.softmax(pred).data.cpu().numpy()
+        return torch.nn.functional.softmax(pred[0], dim=0)
+        # return F.softmax(pred).data.cpu().numpy()
 
     num_batches = math.ceil(len(images) / batch_size)
     i = 0
